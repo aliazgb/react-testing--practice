c46a5e814c88dfc3bdb454db0c6b55ceb10d42df
Ali Azarkashb <ali.azgb10@gmail.com>
2025-05-08 00:47:19 +0200

    add success and error tests for User component using MSW

--- src/components/user/User.test.jsx
+++ src/components/user/User.test.jsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
+import { http } from "msw";
 import { describe, expect, test } from "vitest";
+import { server } from "../mocks/server";
 import User from "./User";
 
 describe("users", () => {
@@ -7,5 +9,20 @@ describe("users", () => {
     render(<User />);
     const heading = screen.getByText("users");
     expect(heading).toBeInTheDocument();
+  });
+  test("should have 3 user item", async () => {
+    render(<User />);
+    const users = await screen.findAllByRole("listitem");
+    expect(users).toHaveLength(3);
+  });
+  test("render error message", async () => {
+    server.use(
+      http.get("https://jsonplaceholder.typicode.com/users", () => {
+        return HttpResponse.json(null, { status: 404 });
+      })
+    );
+    render(<User />);
+    const users = await screen.findByText("Error fetching User");
+    expect(users).toBeInTheDocument();
   });
 });
